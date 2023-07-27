f6f478e66f020ffe362013387882513495ac6819
Rulios <robert_lu20@hotmail.com>
2023-07-27 16:18:05 -0500

    Updated test I2C

--- metrobus-client/test-i2c.py
+++ metrobus-client/test-i2c.py
@@ -1,18 +1,17 @@
-import time
-import pn532pi.nfc as nfc
+from pn532pi import Pn532I2c, Pn532
+
+i2c = Pn532I2c(1)
+nfc = Pn532(i2c)
 
 def main():
     try:
         # Initialize the PN532 module over I2C
-        pn532 = nfc.PN532_I2C(1)  # Replace 1 with the I2C bus number
-
-        # Initialize the PN532 module
-        pn532.SAM_configuration()
+        nfc.begin()
 
         # Wait for an NFC card to be detected
         print("Hold an NFC card near the PN532...")
         while True:
-            uid = pn532.read_passive_target()
+            uid = nfc.read_passive_target()
             if uid is not None:
                 break
 
@@ -24,7 +23,7 @@ def main():
 
         # Write a sample ID (4 bytes) to the NFC card
         sample_id = [0x12, 0x34, 0x56, 0x78]
-        pn532.mifare_classic_write_block(4, sample_id)
+        nfc.mifare_classic_write_block(4, sample_id)
 
         print("Sample ID (", sample_id, ") written to the NFC card.")
 
@@ -33,4 +32,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
