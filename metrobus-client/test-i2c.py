29d89ef5282407a54cc6d4152b9d433e4e648551
Rulios <robert_lu20@hotmail.com>
2023-08-02 01:27:03 -0500

    Added real code papa

--- metrobus-client/test-i2c.py
+++ metrobus-client/test-i2c.py
@@ -1,18 +1,31 @@
 """
-    This examples attempts to take a Mifare Classic 1K card that has been
-    formatted for NDEF messages using mifareclassic_formatndef, and resets
-    the authentication keys back to the Mifare Classic defaults
+    This example will wait for any ISO14443A card or tag, and
+    depending on the size of the UID will attempt to read from it.
+   
+    If the card has a 4-byte UID it is probably a Mifare
+    Classic card, and the following steps are taken:
+   
+    - Authenticate block 4 (the first block of Sector 1) using
+      the default KEYA of 0XFF 0XFF 0XFF 0XFF 0XFF 0XFF
+    - If authentication succeeds, we can then read any of the
+      4 blocks in that sector (though only block 4 is read here)
 
-    To enable debug message, define DEBUG in nfc/pn532_debug.h
+    If the card has a 7-byte UID it is probably a Mifare
+    Ultralight card, and the 4 byte pages can be read directly.
+    Page 4 is read by default since this is the first 'general-
+    purpose' page on the tags.
+
+    To enable debug message, define DEBUG in nfc/pn532_log.h
 """
-import binascii
 import time
+import binascii
 
-from pn532pi import pn532, Pn532
-from pn532pi import Pn532Hsu
+from pn532pi import Pn532, pn532
 from pn532pi import Pn532I2c
 from pn532pi import Pn532Spi
+from pn532pi import Pn532Hsu
 
+# Set the desired interface to True
 SPI = False
 I2C = False
 HSU = True
@@ -27,123 +40,94 @@ elif HSU:
 
 # When the number after #if & #elif set as 0, it will be switch to I2C Mode
 elif I2C:
-    PN532_I2C = Pn532I2c(Pn532I2c.RPI_BUS1)
+    PN532_I2C = Pn532I2c(1)
     nfc = Pn532(PN532_I2C)
 
 
-NR_SHORTSECTOR          = 32    # Number of short sectors on Mifare 1K/4K
-NR_LONGSECTOR           = 8     # Number of long sectors on Mifare 4K
-NR_BLOCK_OF_SHORTSECTOR = 4     # Number of blocks in a short sector
-NR_BLOCK_OF_LONGSECTOR  = 16    # Number of blocks in a long sector
-
-# Determine the sector trailer block based on sector number
-def BLOCK_NUMBER_OF_SECTOR_TRAILER(sector):
-  return (sector)*NR_BLOCK_OF_SHORTSECTOR + NR_BLOCK_OF_SHORTSECTOR-1 if sector < NR_SHORTSECTOR else \
-    NR_SHORTSECTOR*NR_BLOCK_OF_SHORTSECTOR + (sector-NR_SHORTSECTOR)*NR_BLOCK_OF_LONGSECTOR + NR_BLOCK_OF_LONGSECTOR-1
-
-# Determine the sector's first block based on the sector number
-def BLOCK_NUMBER_OF_SECTOR_1ST_BLOCK(sector):
-  (sector) * NR_BLOCK_OF_SHORTSECTOR if sector < NR_SHORTSECTOR else \
-    NR_SHORTSECTOR * NR_BLOCK_OF_SHORTSECTOR + (sector - NR_SHORTSECTOR) * NR_BLOCK_OF_LONGSECTOR
-
-
-# The default Mifare Classic key
-KEY_DEFAULT_KEYAB = bytearray([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
-
 def setup():
-  print("-------Looking for PN532--------")
+    nfc.begin()
 
-  nfc.begin()
+    versiondata = nfc.getFirmwareVersion()
+    if (not versiondata):
+        print("Didn't find PN53x board")
+        raise RuntimeError("Didn't find PN53x board")  # halt
 
-  versiondata = nfc.getFirmwareVersion()
-  if not versiondata:
-    print("Didn't find PN53x board")
-    raise RuntimeError("Didn't find PN53x board")  # halt
+    #  Got ok data, print it out!
+    print("Found chip PN5 {:#x} Firmware ver. {:d}.{:d}".format((versiondata >> 24) & 0xFF, (versiondata >> 16) & 0xFF,
+                                                                (versiondata >> 8) & 0xFF))
 
-  # Got ok data, print it out!
-  print("Found chip PN5 {:#x} Firmware ver. {:d}.{:d}".format((versiondata >> 24) & 0xFF, (versiondata >> 16) & 0xFF,
-                                                             (versiondata >> 8) & 0xFF))
+    #  configure board to read RFID tags
+    nfc.SAMConfig()
 
-  # configure board to read RFID tags
-  nfc.SAMConfig()
-  
+    print("Waiting for an ISO14443A Card ...")
+
+
 def loop():
-  authenticated = False               # Flag to indicate if the sector is authenticated
-  blankAccessBits = bytearray([0xff, 0x07, 0x80 ])
-  numOfSector = 16                 # Assume Mifare Classic 1K for now (16 4-block sectors)
-  
-  print("Place your NDEF formatted Mifare Classic 1K card on the reader")
-  # Wait for user input before proceeding
-  input("and press any key to continue ...")
-  
-    
-  # Wait for an ISO14443A type card (Mifare, etc.).  When one is found
-  # 'uid' will be populated with the UID, and uidLength will indicate
-  # if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
-  success, uid = nfc.readPassiveTargetID(pn532.PN532_MIFARE_ISO14443A_106KBPS)
+    #  Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+    #  'uid' will be populated with the UID, and uidLength will indicate
+    #  if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+    success, uid = nfc.readPassiveTargetID(pn532.PN532_MIFARE_ISO14443A_106KBPS)
 
-  if (success): 
-    # We seem to have a tag ...
-    # Display some basic information about it
-    print("Found an ISO14443A card")
-    print("UID Length: {:d}".format(len(uid)))
-    print("UID Value: {}".format(binascii.hexlify(uid)))
+    if (success):
+        #  Display some basic information about the card
+        print("Found an ISO14443A card")
+        print("UID Length: {:d}".format(len(uid)))
+        print("UID Value: {}".format(binascii.hexlify(uid)))
 
-    
-    # Make sure this is a Mifare Classic card
-    if (len(uid) != 4):
-      print("Ooops ... this doesn't seem to be a Mifare Classic card!") 
-      return
-    
-    print("Seems to be a Mifare Classic card (4 byte UID)")
-    print("")
-    print("Reformatting card for Mifare Classic (please don't touch it!) ... ")
+        if (len(uid) == 4):
+            #  We probably have a Mifare Classic card ...
+            print("Seems to be a Mifare Classic card (4 byte UID)")
 
-    # Now run through the card sector by sector
-    for idx in range(numOfSector):
-      # Step 1: Authenticate the current sector using key B 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
-      success = nfc.mifareclassic_AuthenticateBlock (uid, BLOCK_NUMBER_OF_SECTOR_TRAILER(idx), 1, KEY_DEFAULT_KEYAB)
-      if (not success):
-        print("Authentication failed for sector {}".format(numOfSector))
-        return
-      
-      # Step 2: Write to the other blocks
-      blockBuffer = bytearray('\x00'*16)
-      if (idx == 16):
-        if (not (nfc.mifareclassic_WriteDataBlock((BLOCK_NUMBER_OF_SECTOR_TRAILER(idx)) - 3, blockBuffer))):
-          print("Unable to write to sector {}".format(numOfSector))
-          return
-      if ((idx == 0) or (idx == 16)):
-        if (not (nfc.mifareclassic_WriteDataBlock((BLOCK_NUMBER_OF_SECTOR_TRAILER(idx)) - 2, blockBuffer))):
-          print("Unable to write to sector {}".format(numOfSector))
-          return
-      else:
-        if (not (nfc.mifareclassic_WriteDataBlock((BLOCK_NUMBER_OF_SECTOR_TRAILER(idx)) - 3, blockBuffer))):
-          print("Unable to write to sector {}".format(numOfSector))
-          return
-        if (not (nfc.mifareclassic_WriteDataBlock((BLOCK_NUMBER_OF_SECTOR_TRAILER(idx)) - 2, blockBuffer))):
-          print("Unable to write to sector {}".format(numOfSector))
-          return
+            #  Now we need to try to authenticate it for read/write access
+            #  Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
+            print("Trying to authenticate block 4 with default KEYA value")
+            keya = bytearray([0xD3, 0xF7, 0xD3, 0xF7, 0xD3, 0xF7])
 
-      blockBuffer = bytearray('\x00' * 16)
-      if (not(nfc.mifareclassic_WriteDataBlock((BLOCK_NUMBER_OF_SECTOR_TRAILER(idx)) - 1, blockBuffer))):
-        print("Unable to write to sector {}".format(numOfSector))
-        return
-      
-      # Step 3: Reset both keys to 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
-      blockBuffer = KEY_DEFAULT_KEYAB + blankAccessBits + b'\x69' + KEY_DEFAULT_KEYAB
+            #  Start with block 4 (the first block of sector 1) since sector 0
+            #  contains the manufacturer data and it's probably better just
+            #  to leave it alone unless you know what you're doing
+            success = nfc.mifareclassic_AuthenticateBlock(uid, 4, 0, keya)
 
-      # Step 4: Write the trailer block
-      if (not (nfc.mifareclassic_WriteDataBlock((BLOCK_NUMBER_OF_SECTOR_TRAILER(idx)), blockBuffer))):
-        print("Unable to write trailer block of sector ")
-        print(numOfSector)
-        return
-  
-  # Wait a bit before trying again
-  print("\n\nDone!")
-  time.sleep(1)
+            if (success):
+                print("Sector 1 (Blocks 4..7) has been authenticated")
+
+                #  If you want to write something to block 4 to test with, uncomment
+                #  the following line and this text should be read back in a minute
+                # data = bytearray([ 'a', 'd', 'a', 'f', 'r', 'u', 'i', 't', '.', 'c', 'o', 'm', 0, 0, 0, 0])
+                # success = nfc.mifareclassic_WriteDataBlock (4, data)
+
+                #  Try to read the contents of block 4
+                success, data = nfc.mifareclassic_ReadDataBlock(4)
+
+                if (success):
+                    #  Data seems to have been read ... spit it out
+                    print("Reading Block 4: {}".format(binascii.hexlify(data)))
+                    return True
+
+                else:
+                    print("Ooops ... unable to read the requested block.  Try another key?")
+            else:
+                print("Ooops ... authentication failed: Try another key?")
+
+        elif (len(uid) == 7):
+            #  We probably have a Mifare Ultralight card ...
+            print("Seems to be a Mifare Ultralight tag (7 byte UID)")
+
+            #  Try to read the first general-purpose user page (#4)
+            print("Reading page 4")
+            success, data = nfc.mifareultralight_ReadPage(4)
+            if (success):
+                #  Data seems to have been read ... spit it out
+                binascii.hexlify(data)
+                return True
+
+            else:
+                print("Ooops ... unable to read the requested page!?")
+
+    return False
 
 if __name__ == '__main__':
     setup()
-    while True:
-      loop()
+    found = loop()
+    while not found:
+        found = loop()
